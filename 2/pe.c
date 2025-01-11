a5bb3109a99971721fcf212316875cbcb8ae2d2f
frei480 <frei741@gmail.com>
2025-01-12 00:53:10 +0500

    fix pe

--- 2/pe.c
+++ 2/pe.c
@@ -1,49 +1,41 @@
-/*
-int is_prime(int n){
-    if (n == 2 || n == 3) return 1;
-    if ((n < 2) || ((n % 2) == 0) || ((n % 3) == 0)) return 0;
-	for(int j = 5; j*j <= n; j += 6)
-		if (( (n % j) == 0) || ((n % (j+2))==0))
-		    return 0;
-	return 1;
-}
-*/
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 struct sieve_t {
-    unsigned long long n;           
+    int n;           
     unsigned char *s;
 };
 
+void set_prime(struct sieve_t *sv, unsigned n){
+    int bytenum = n/8;
+    int bitnum = n % 8;
+    sv->s[bytenum] |= (1 << bitnum);
+}
+
+int is_prime(struct sieve_t *sv, unsigned n) {
+    int bytenum = n/8;
+    int bitnum = n % 8;
+    int x = (sv->s[bytenum] >> bitnum) & 1;
+    return (x==0)? 1 : 0;
+}
 
 void fill_sieve(struct sieve_t *sv) {    
-    
-    unsigned long long i, j;
-    sv->s[0] |= (1 << 0);
-    sv->s[0] |= (1 << 1);
-
-    for (i = 2; i * i <= (sv->n*8); i++) {
-        if (!(sv->s[i / 8] & (1 << (i % 8)))) { 
-            for (j = i * i; j <= sv->n; j += i) {
-                sv->s[j / 8] |= (1 << (j % 8)); 
-            }
-        }
-    }
+    for (unsigned i = 2; i * i <= sv->n*8+1; i++) 
+        if (is_prime(sv, i))  
+            for (unsigned j = i * i; j <= sv->n*8+1; j += i) 
+                set_prime(sv, j);         
+   
 }
 
-int is_prime(struct sieve_t *sv, unsigned long long n) {
-    return !((sv->s[n / 8] >> n % 8) & 1);
-}
 void pn(struct sieve_t *sv, unsigned long long n) {	
-	unsigned long long prime_no=1, prime = 2;		  
+	unsigned prime_no=1, prime = 2;		  
 	while (prime_no != n )
 	{		
 		if(is_prime(sv, ++prime))
 			prime_no++;
 	}
-	printf("%llu", prime);
+	printf("%u", prime);
 }
 int main() {
     int N;
@@ -51,7 +43,7 @@ int main() {
 	int res;
     struct sieve_t sv;
     sv.n = 179424673/8+1;
-    sv.s = calloc(sv.n ,sizeof(char)); 
+    sv.s = (char*)calloc(sv.n/8 ,sizeof(char)); 
     fill_sieve(&sv);
 
 	res = scanf("%llu", &n);
